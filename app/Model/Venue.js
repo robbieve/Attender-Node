5129ebdbf47e20c6ae18ba66ee5b46c0bccd1e21
robbieve <robbieeve751994@gmail.com>
2017-11-16 18:21:25 +0800

    Added info

--- app/Model/Venue.js
+++ app/Model/Venue.js
@@ -18,6 +18,7 @@ let venueSchema = mongoose.Schema({
     index: '2d',
     type: [Number]
   },
+  info: String,
   tag1: String,
   tag2: String,
   image: { type: String, default: 'https://www.scandichotels.com/imagevault/publishedmedia/suw58cmdyrxfvvjep2a5/Scandic-Malmen-Interior-bar-Lilla-hotellbaren-over.jpg' },
