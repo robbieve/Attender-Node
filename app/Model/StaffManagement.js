ce48fa5f18745c1107036c39dea7cd681ee8669e
robbieve <robbieeve751994@gmail.com>
2018-01-04 12:15:53 +0800

    Updated staff management index

--- app/Model/StaffManagement.js
+++ app/Model/StaffManagement.js
@@ -29,9 +29,9 @@ let staffManagementSchema = mongoose.Schema({
   schedules: { type: Mixed, default: {} },
 
   employer: { type: ObjectId, ref: 'Employer' },
-  
+
 })
 
-staffManagementSchema.index({ staff: 1, venue: 1 }, { unique: true })
+staffManagementSchema.index({ staff: 1, employer: 1 }, { unique: true })
 
 module.exports = mongoose.model('StaffManagement', staffManagementSchema)
