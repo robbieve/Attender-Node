00f3ab227ac0041b4ebe51b0ace9d53173c6286b
robbieve <robbieeve751994@gmail.com>
2018-03-13 20:19:30 +0800

    Added DOB when saving staff

--- app/Http/Controllers/Api/EmployerController.js
+++ app/Http/Controllers/Api/EmployerController.js
@@ -6,10 +6,10 @@ const Employer = use('App/Model/Employer')
 const StaffManagement = use('App/Model/StaffManagement')
 const EmployerNotification = use('App/Model/EmployerNotification')
 
-// const Notify = use('App/Serializers/Notify')
+const Notify = use('App/Serializers/Notify')
 
 
-// let notify = new Notify()
+let notify = new Notify()
 
 
 
@@ -265,7 +265,7 @@ class EmployerController {
               type: 'venue-interest'
           })
           res.json({ status: true, venue: employer })
-          // yield notify.venueInterest(req.user.staffId, employer)
+          yield notify.venueInterest(req.user.staffId, employer)
       }else{
           delete employer.interested[req.user._id]
           employer.markModified('interested')
@@ -355,7 +355,7 @@ class EmployerController {
     })
     res.json({ status: true })
     let m = yield Message.findOne({ _id: message._id }).populate('staff', 'fullname').populate('employer', 'name').populate('sender', '_id isStaff isVenue')
-    // yield notify.newMessage(m)
+    yield notify.newMessage(m)
     let update = yield Message.update({ conversation: req.param('convo', '') }, { $pull: { archivedTo: { $in: [req.user._id, req.input('receiver')] } } }, { multi: true })
   }
 
