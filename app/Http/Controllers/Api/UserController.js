edfb2a7a338de527dedfb7f113043ece64aa28e3
robbieve <robbieeve751994@gmail.com>
2018-03-19 21:04:24 +0800

    Fixed save staff profile and start rate issue not a number

--- app/Http/Controllers/Api/UserController.js
+++ app/Http/Controllers/Api/UserController.js
@@ -225,8 +225,8 @@ class UserController {
             staff.preferredDistance = req.input('preferredDistance', staff.preferredDistance)
             staff.frequency = req.input('frequency', staff.frequency)
             staff.position = position || staff.position
-            staff.startRate = req.input('startRate', staff.startRate)
-            staff.endRate = req.input('endRate', staff.endRate)
+            staff.startRate = Number.isNaN(req.input('startRate', staff.startRate))? staff.startRate: req.input('startRate', staff.startRate)
+            staff.endRate = Number.isNaN(req.input('endRate', staff.endRate))? staff.endRate: req.input('endRate', staff.endRate)
             staff.rateBadge = req.input('rateBadge', staff.rateBadge)
             staff.rateType = req.input('rateType', staff.rateType)
             staff.qualifications = qualifications || staff.qualifications
