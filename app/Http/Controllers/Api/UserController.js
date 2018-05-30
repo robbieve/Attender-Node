4117badaa964e9e50db151b43f2b7bac54d5ca7d
robbieve <robbieeve751994@gmail.com>
2018-05-30 16:56:19 +0800

    Update Fix Production

--- app/Http/Controllers/Api/UserController.js
+++ app/Http/Controllers/Api/UserController.js
@@ -23,6 +23,7 @@ class UserController {
 
     * userUpdateAccount(req, res) {
         const assembly_user = yield PromisePay.updateUser(req.user.promiseId, req._body);
+        const userStaff = yield User.findOne(req.user._id).populate('staffId');
         yield Staff.update({
             user: req.user._id
         },
@@ -63,7 +64,7 @@ class UserController {
                 zip: req._body.zip
             },
         }).then(d => {
-            const userStaff = yield User.findOne(req.user._id).populate('staffId');
+            
             return res.json({userStaff, assembly_user});
         }).catch(err => {
             return res.json({status: false, messageCode: 'UPDATE_PROFILE_FAILED', message: 'Porfile Update failed'})
